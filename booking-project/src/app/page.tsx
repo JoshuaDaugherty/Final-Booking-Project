6098ff8a8997ade6f1f8be23686f5b2bcdd7c426
Joshua Daugherty <Joshua_Daugherty@insideranken.org>
2025-02-03 10:49:27 -0600

    initial Commit

---
+++ booking-project/src/app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+   <section className="text-3xl flex min-h-screen justify-center items-center">
+    <h2 className="font-semibold">Booking App</h2>
+   </section>
+  );
+  
+}
